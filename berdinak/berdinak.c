db7fef2416868c10ae4902b1b919b75529907509
mikelmiras <mmiras004@ikasle.ehu.eus>
2023-03-01 21:18:04 +0100

    Documentation

--- berdinak/berdinak.c
+++ berdinak/berdinak.c
@@ -1,21 +1,27 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main(int argc, char *argv[]) {
     int fd1, fd2;
     char buffer1, buffer2;
     int offset1 = 0, offset2 = 0;
-
+ 	
+    char* fileerr = "Fitxategia ezin da zabaldu";
+    // Argumentu guztiak (2 fitxategiak) ez badira zehaztu, bueltatu
     if (argc != 3) {
         exit(1);
     }
-
+	
+    // Zabaldu 1. fitxategia
     fd1 = open(argv[1], O_RDONLY);
+    // 1. fitxategia ezin bada zabaldu, bueltatu
     if (fd1 < 0) {
+	write(STDERR_FILENO, fileerr, strlen(fileerr));
         exit(1);
     }
-
+ 
+    // 1. fitxategiko gauza bera
     fd2 = open(argv[2], O_RDONLY);
     if (fd2 < 0) {
         close(fd1);
